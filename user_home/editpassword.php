9290d748da42c7bff3e47594f203466d8aba8a29
Ketan Bagal <kketann70@gmail.com>
2017-10-09 12:24:04 +1300

    Add files via upload

--- user_home/editpassword.php
+++ user_home/editpassword.php
@@ -56,8 +56,8 @@
 
 </fieldset>
 
-<?php if(isset($_GET['edit'])) {echo "<input type='submit' name='submit' value='submit'>";$_SESSION['updatingid']=$_SESSION['uid'];}
-else {echo "<input type='submit' name='password' value='submit'>";}
+<?php if(isset($_GET['edit'])) {echo "<input type='submit' name='submit' value='Submit'>";$_SESSION['updatingid']=$_SESSION['uid'];}
+else {echo "<input type='submit' name='password' value='Submit'>";}
   ?>
 
 </form>
